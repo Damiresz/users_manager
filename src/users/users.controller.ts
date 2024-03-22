f2880c15090b0f22552644501353ba2d1148f9b8
damiresz <damirabd879@gmail.com>
2024-03-22 21:59:53 +0100

    del prisma

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,6 @@
 import { BadRequestException, Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateUserDto } from './dto/create_user.dto';
 import { UsersService } from './users.service';
-import { User } from '@prisma/client';
 
 @Controller('users')
 export class UsersController {
@@ -12,13 +11,13 @@ export class UsersController {
   @UsePipes(new ValidationPipe)
   @Post()
   create(@Body() createUser: CreateUserDto) {
-    return this.userService.createUser(createUser);
+    return 
   }
 
 
   @Get()
-  getUsers():Promise<User[]> {
-    return this.userService.getUsers();
+  getUsers() {
+    return 
   }
 
 
